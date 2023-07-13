371ed255297e311664ee99ef54497e3acda3b674
guillermo <guillermo18zgordon@gmail.com>
2023-07-13 09:07:54 -0500

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,55 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
 
 export default function App() {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+
+  useEffect(() => {
+    const requestLocationPermission = async () => {
+      try {
+        const { state } = await navigator.permissions.query({
+          name: "geolocation",
+        });
+
+        if (state === "granted") {
+          fetchLocation();
+        } else if (state === "prompt") {
+          await navigator.geolocation.getCurrentPosition(
+            (position) => {
+              setLatitude(position.coords.latitude);
+              setLongitude(position.coords.longitude);
+            },
+            (error) => {
+              console.error("Error fetching geolocation:", error);
+            }
+          );
+        } else {
+          console.error("Location permission denied.");
+        }
+      } catch (error) {
+        console.error("Error requesting location permission:", error);
+      }
+    };
+
+    requestLocationPermission();
+  }, []);
+
+  const fetchLocation = () => {
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        setLatitude(position.coords.latitude);
+        setLongitude(position.coords.longitude);
+      },
+      (error) => {
+        console.error("Error fetching geolocation:", error);
+      }
+    );
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <div>
+      <p>Latitude: {latitude}</p>
+      <p>Longitude: {longitude}</p>
+    </div>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
